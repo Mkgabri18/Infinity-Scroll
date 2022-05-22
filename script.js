d6d69f4a69cec9b2c81d8bc5fd907ceaedb021e9
Mkgabri18 <carrasco.gabriel136@gmail.com>
2022-05-22 02:27:51 +0200

    declare variable

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
 let ready = false
+let imagesLoaded = 0
 let totalImages = 0
 let photosArray = []
 let initialLoad = true
